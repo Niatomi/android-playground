d7facaca5534607dcc29e4209a18ba10899a8c8a
Niatomi <playervoker@gmail.com>
2024-04-17 23:46:17 +0400

    Add constraint layout dependency

--- allInOne/app/build.gradle.kts
+++ allInOne/app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.constraintlayout:constraintlayout-compose-android:1.1.0-alpha13")
 
     testImplementation("junit:junit:4.13.2")
 
